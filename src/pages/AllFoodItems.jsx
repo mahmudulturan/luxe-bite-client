f9cd4a72f02982729e09b2c427a1e6fdcd6b8635
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2023-11-07 11:08:04 +0600

    Feat: Search functionality added on all items page

--- src/pages/AllFoodItems.jsx
+++ src/pages/AllFoodItems.jsx
@@ -6,20 +6,24 @@ import { useState } from "react";
 import Loading from "../components/Loading/Loading";
 
 const AllFoodItems = () => {
-    const [currentPage, setCurrentPage] = useState(0)
-    const [perPageCount, setPerPageCount] = useState(9)
+    const [currentPage, setCurrentPage] = useState(0);
+    const [perPageCount, setPerPageCount] = useState(9);
+    const [searchKey, setSearchKey] = useState("");
     const axios = useAxios();
+    const url = `/all-food-items?page=${currentPage}&limit=${perPageCount}&search=${searchKey}`;
     const { data, isLoading } = useQuery({
-        queryKey: ["allfoodItems", currentPage, perPageCount], queryFn: async () => {
-            const res = await axios.get(`/all-food-items?page=${currentPage}&limit=${perPageCount}`)
+        queryKey: ["allfoodItems", currentPage, perPageCount, url], queryFn: async () => {
+            const res = await axios.get(url)
             return res;
         }
     })
-    if(isLoading){
-        return <Loading></Loading>
+    const handleSearch = () => {
+        const searchKeyword = document.getElementById("search_key").value;
+        setSearchKey(searchKeyword);
     }
+
     const productsCount = data?.data.count;
-    const pageCount = Math.ceil(productsCount/perPageCount)
+    const pageCount = Math.ceil(productsCount / perPageCount)
     const totalButton = [...Array(pageCount || []).keys()]
 
     return (
@@ -30,7 +34,8 @@ const AllFoodItems = () => {
                     <div className="join flex-col md:flex-row">
                         <div>
                             <div>
-                                <input className="input join-item outline-none border-x-0 border-t-0 rounded-none border-secondaryCol border-b-2 bg-transparent focus:outline-none focus:bg-white transition-colors duration-200" placeholder="Search" />
+                                <input className="input join-item outline-none border-x-0 border-t-0 rounded-none border-secondaryCol border-b-2 bg-transparent focus:outline-none focus:bg-white transition-colors duration-200" placeholder="Search" id="search_key" />
+        
                             </div>
                         </div>
                         <select className="select join-item rounded-none border-accentCol border-none focus:outline-none bg-secondaryCol text-darkSecondary hover:text-white font-semibold hover:bg-darkSecondary">
@@ -41,25 +46,30 @@ const AllFoodItems = () => {
                             <option>Gosto Bhat</option>
                         </select>
                         <div className="indicator">
-                            <button className="btn join-item bg-secondaryCol text-darkSecondary hover:text-white hover:bg-darkSecondary border-accentCol border-none focus:outline-none">Search</button>
+                            <button onClick={handleSearch} className="btn join-item bg-secondaryCol text-darkSecondary hover:text-white hover:bg-darkSecondary border-accentCol border-none focus:outline-none">Search</button>
                         </div>
                     </div>
                 </div>
 
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-10">
-                    {
-                        data?.data.result.map(item => <FoodCard key={item._id} item={item}></FoodCard>)
-                    }
-                </div>
+                {
+                    isLoading ?
+                        <Loading></Loading>
+                        :
+                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-10">
+                            {
+                                data?.data.result.map(item => <FoodCard key={item._id} item={item}></FoodCard>)
+                            }
+                        </div>
+                }
 
                 <div className="mb-10 text-center">
                     <div className="join">
-                        <button onClick={()=> currentPage>0 && setCurrentPage(currentPage-1)} className="join-item btn bg-secondaryCol/60 outline-none border-none">Prev</button>
+                        <button onClick={() => currentPage > 0 && setCurrentPage(currentPage - 1)} className="join-item btn bg-secondaryCol/60 outline-none border-none">Prev</button>
                         {
-                            totalButton.map((button,indx)=> <button onClick={()=> setCurrentPage(button)} key={indx} className="join-item btn bg-secondaryCol/60 outline-none border-none">{button+1}</button>)
+                            totalButton.map((button, indx) => <button onClick={() => setCurrentPage(button)} key={indx} className="join-item btn bg-secondaryCol/60 outline-none border-none">{button + 1}</button>)
                         }
-                        
-                        <button onClick={()=> currentPage<totalButton?.length-1 && setCurrentPage(currentPage+1)} className="join-item btn bg-secondaryCol/60 outline-none border-none">Next</button>
+
+                        <button onClick={() => currentPage < totalButton?.length - 1 && setCurrentPage(currentPage + 1)} className="join-item btn bg-secondaryCol/60 outline-none border-none">Next</button>
                     </div>
                 </div>
 
